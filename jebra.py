92a25da12ef9db90be9a55928c2fd4479795a225
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-29 14:30:13 +0530

    updated requirements.

--- jebra.py
+++ jebra.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 """jebra.py: 
 
+NOTE: The raw image which we operate on is 4x time larger in width to make sure
+we get right number of pixel to offset. The figure which is displayed is
+downsampled 4x. Otherwise the picture flow will not be smooth.
 
 """
 from __future__ import division, print_function
@@ -18,6 +21,7 @@ import os
 import numpy as np
 import time
 import random
+import scipy.misc
 
 try:
     import Tkinter as tk
@@ -26,7 +30,9 @@ except ImportError as e:
 
 from PIL import Image, ImageTk
 
-realW_, w_, h_ = 800, 800, 480
+step_ = 0
+start_      = True
+w_, h_, ws_ = 800, 480, 1
 img_           = None
 tkImage_       = None
 speed_         = 10               # mm in seconds
@@ -36,7 +42,7 @@ root_          = tk.Tk()
 label_         = None
 canvas_        = tk.Canvas( root_, width=w_, height=h_ )
 imgOnCanvas_   = None
-T_             = 5    # in ms
+T_             = 20    # in ms
 nrows_         = 10
 density_       = ((w_**2 + h_**2)**0.5)/5/25.4   # per mm
 
@@ -48,6 +54,8 @@ print( '[WARN] This is customized for 5" LCD display. The values you'
 
 def im2tkimg( img ):
     global realW_
+    h, w  = img.shape 
+    img = scipy.misc.imresize(img, (h, int(w*ws_)))
     return ImageTk.PhotoImage(Image.fromarray(img))
 
 def mm2px( mm ):
@@ -62,18 +70,20 @@ def init_arrays():
     # numpy and opencv has incompatible coordinate system. So silly.
     print( 'I', end = '' )
     sys.stdout.flush()
-    img_ = np.dstack([np.zeros((h_, w_), dtype = np.uint8) 
-        for i in range(3)])
-    stride = mm2px( slitWidth_ )
-    print( stride )
-    for i in range( 0, w_, 2*stride ):
-        img_[:,i:i+stride,:] = 255
-    tkImage_ = im2tkimg(img_)
+    img_ = np.zeros( (h_, w_ * ws_), dtype=np.uint8 )
+    stride = ws_ * mm2px( slitWidth_ )
+    for i in range( 0, w_ * ws_, 2*stride ):
+        img_[:,i:i+stride] = 255
+    tkImage_ = im2tkimg(img_ )
 
 def generate_stripes( offset ):
     global speed_, slitWidth_ 
     global tkImage_, canvas_
     global img_
+    if not start_:
+        print( 'x', end = '' )
+        sys.stdout.flush()
+        return 
     offset = int( offset )
     img_ = np.roll( img_, (0,offset,0), axis=1)
     tkImage_ = im2tkimg( img_ )
@@ -91,15 +101,20 @@ def update_frame( ):
     global speed_, slitWidth_, density_
     global t_, label_, root_
     global canvas_, imgOnCanvas_
-    #  print( 'Time = %.3f ' % t_ )
-    offset = speed_ * density_ * (time.time() - t_)
-    generate_stripes( intOffset(offset) )
+    global step_
+    step_ += 1
+    dt = time.time() - t_
     t_ = time.time()
+    offset = speed_ * density_ * dt
+    s = offset / density_ / ws_ / dt 
+    if step_ % 25 == 0:
+        print( 'OFFSET %d, dt=%.2f ms speed=%.2f mm/s' % ( offset, dt*1000, s ) )
+    generate_stripes( intOffset(offset) )
     root_.after( T_, update_frame )
 
 def speed_changed( newspeed ):
     global speed_ 
-    newspeed = int(newspeed)
+    newspeed = ws_ * int(newspeed)
     speed_ = newspeed
     print( 'New speed %d' % newspeed )
     return True
@@ -111,11 +126,15 @@ def width_changed( width ):
     init_arrays()
     return True
 
+def toggle_start_stop( ):
+    global start_
+    start_ = not start_
+
 def init_tk():
     global tkImage_, root_
     global canvas_, imgOnCanvas_
     global label_
-    canvas_.grid(row=0, column=0, columnspan=2, rowspan = nrows_)
+    canvas_.grid(row=0, column=0, columnspan=3, rowspan = nrows_)
     init_arrays()
     assert tkImage_
     imgOnCanvas_ = canvas_.create_image( 0, 0
@@ -124,25 +143,35 @@ def init_tk():
             )
 
     # add speed scale.
-    speed = tk.Scale(root_, from_ = 10, to_ = 100
+    speed = tk.Scale(root_, from_ = 5, to_ = 20
+                , label = 'mm/s'
                 , command= lambda v: speed_changed(v)
             )
     speed.config(orient = tk.HORIZONTAL)
     speed.grid(row=nrows_, column=0 )
+    speed.set( speed_ )
 
     width = tk.Scale(root_, from_ = 2, to_ = 10
+                , label = 'mm'
                 , command= lambda v: width_changed(v)
             ) 
     width.set( slitWidth_ )
     width.config(orient = tk.HORIZONTAL)
     width.grid(row=nrows_, column = 1 )
 
+    # toggle step/stop
+    startStop = tk.Button( text='Start/Stop' 
+            , command = toggle_start_stop
+            )
+    startStop.grid( row=nrows_, column=2 )
+
+
 
 def main():
     global root_, t_
     t_ = time.time() 
     init_tk()
-    root_.after( 100, update_frame )
+    root_.after( T_, update_frame )
     root_.mainloop()
 
 if __name__ == '__main__':
