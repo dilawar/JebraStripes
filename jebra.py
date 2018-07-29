76b2fd663362b255cf0686638ee410be5cd96022
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-29 11:08:37 +0530

    Refresh every 20ms.

--- jebra.py
+++ jebra.py
@@ -17,8 +17,7 @@ import os
 import numpy as np
 import screeninfo
 import time
-import gi
-import signal
+import cv2
 
 try:
     import Tkinter as tk
@@ -27,25 +26,33 @@ except ImportError as e:
 
 from PIL import Image, ImageTk
 
-w_, h_     = 0, 0
-img_       = None
-tkImage_   = None
-speed_     = 100            # pixel in seconds
-slitWidth_ = 100        # in pixels.
-t_         = time.time()
-root_      = tk.Tk()
-label_     = None
+realW_, w_, h_ = 0, 0, 0
+img_           = None
+tkImage_       = None
+speed_         = 100            # pixel in seconds
+slitWidth_     = 20             # in pixels.
+t_             = time.time()
+root_          = tk.Tk()
+label_         = None
+T_             = 20    # in ms
 
 def set_resolution():
     global w_, h_
     global img_
     monitor = screeninfo.get_monitors()[-1]
-    w_ = monitor.width  // 2
-    h_ = monitor.height // 2
-    # make sure that width is divisible by slitWidth_ and must be even.
-    #  nSlits = w_ // slitWidth_
-    #  nSlits = nSlits - (nSlits % 2 ) + 1
-    #  w_ = nSlits * slitWidth_
+    w_ = monitor.width // 2
+    realW_ = w_
+    h_ = monitor.height  // 2
+    # make sure that width is divisible by slitWidth_ and must be even and
+    # slightly larger than the screen width so we don't see uneven gaps.
+    nSlits = w_ // slitWidth_
+    nSlits = nSlits - (nSlits % 2 ) + 1
+    w_ = nSlits * slitWidth_
+    print( 'New dim for image %d, monitory width %d' % (w_, realW_))
+
+def im2tkimg( img ):
+    global realW_
+    return ImageTk.PhotoImage(Image.fromarray( img[:,0:realW_-1,:]))
 
 def init_arrays():
     global img_
@@ -54,42 +61,29 @@ def init_arrays():
     print( 'I', end = '' )
     sys.stdout.flush()
     img_ = np.dstack([np.zeros( (h_, w_), dtype = np.uint8) for i in range(3)])
-    print( img_.shape )
+    #  print( img_.shape )
     for i in range( 0, w_, 2*slitWidth_ ):
         img_[:,i:i+slitWidth_,:] = 255
-
-    im = Image.fromarray( img_ )
-    im.save( "__background.png" )
-    tkImage_   = ImageTk.PhotoImage( im )
-
-def show_frame(  waitFor = 1 ):
-    global img_
-    global windowName_
-    cv2.imshow( windowName_, img_ )
-    cv2.waitKey( waitFor )
+    tkImage_ = im2tkimg(img_)
 
 def generate_stripes( offset = 0 ):
     global speed_, slitWidth_ 
     global tkImage_, label_
     global img_
-
     offset = int( offset )
-    img_ = np.roll( img_, (0,offset,0), axis=2)
-    tkImage_ = ImageTk.PhotoImage( Image.fromarray(img_) )
+    img_ = np.roll( img_, (0,offset,0), axis=1)
+    tkImage_ = im2tkimg( img_ )
     label_.config( image = tkImage_ )
-
-def __init__(self):
-    pass
 
 def update_frame( ):
     global img_
     global speed_, slitWidth_
     global t_, label_, root_
+    print( 'Time = %.3f ' % t_ )
     offset = int( speed_ * (time.time() - t_))
     t_ = time.time()
     generate_stripes( offset )
-    print( 'updating frame' )
-    root_.after( 100, update_frame )
+    root_.after( T_, update_frame )
 
 def speed_changed( self, event ):
     global speed_ 
